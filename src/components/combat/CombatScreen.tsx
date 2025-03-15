591f74348687669536c3a8cd1702e0bc08dee18b
DonnervS <166853186+DonnervS@users.noreply.github.com>
2025-03-15 23:01:42 +0100

    Initial code commit with combat system implementation

---
+++ src/components/combat/CombatScreen.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { Model, WeaponStats } from '../../types/gameTypes'
+import ModelCard from './ModelCard'
+import DiceDisplay, { DieResult } from './DiceDisplay'
+import ActionButtons from './ActionButtons'
+import CombatLog from './CombatLog'
+
+// ... Rest of the file content ...
